f5b48849a3cbad6a1bbca69ce7b2d149bc7afcee
eme-ce <matias.croci@gmail.com>
2024-08-25 19:17:34 -0300

    modified:   scripts/aliases.sh

--- scripts/gammastep.sh
+++ scripts/gammastep.sh
@@ -2,13 +2,13 @@
 
 mkdir -p ~/.config/gammastep
     cat <<EOF | tee ~/.config/gammastep/config.ini
-    [general]
-    temp-day=5800
-    temp-night=3200
-    gamma=0.9
-    adjustment-method=randr
-    location-provider=manual
-    [manual]
-    lat=-34.603722
-    lon=-58.381592
+[general]
+temp-day=5800
+temp-night=3200
+gamma=0.9
+adjustment-method=randr
+location-provider=manual
+[manual]
+lat=-34.603722
+lon=-58.381592
 EOF
